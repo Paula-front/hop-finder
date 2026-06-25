0d128bef60a2799a27d8ee4e5147de31abd58f45
Paula-front <TU_CORREO_DE_PAULA_FRONT>
2026-06-25 16:20:18 -0400

    refactor: create reusable beer style card

---
+++ src/components/beer/StyleCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="style-card" :class="`style-card--${style.slug}`">
+    <div class="style-card__top">
+      <span class="style-card__icon">{{ style.icon }}</span>
+      <span class="style-card__range">{{ style.range }}</span>
+    </div>
+
+    <h3>{{ style.name }}</h3>
+    <p>{{ style.description }}</p>
+
+    <div class="style-card__details">
+      <span>{{ style.profile }}</span>
+      <span>{{ style.alcohol }}</span>
+    </div>
+
+    <RouterLink :to="`/beers?style=${style.slug}`" class="style-card__link">
+      Explorar →
+    </RouterLink>
+  </article>
+</template>
+
+<script setup>
+defineProps({
+  style: {
+    type: Object,
+    required: true
+  }
+})
+</script>
--- src/components/home/StyleSection.vue
+++ src/components/home/StyleSection.vue
@@ -7,50 +7,72 @@
     </div>
 
     <div class="style-grid">
-      <article
+      <StyleCard
         v-for="style in beerStyles"
-        :key="style.name"
-        class="style-card"
-      >
-        <span class="style-card__icon">{{ style.icon }}</span>
-        <h3>{{ style.name }}</h3>
-        <p>{{ style.description }}</p>
-      </article>
+        :key="style.slug"
+        :style="style"
+      />
     </div>
   </section>
 </template>
 
 <script setup>
+import StyleCard from '../beer/StyleCard.vue'
+
 const beerStyles = [
   {
+    slug: 'ipa',
     name: 'IPA',
     icon: '🌿',
-    description: 'Aromática, lupulada y con amargor marcado.'
+    range: 'IBU alto',
+    profile: 'Lupulada',
+    alcohol: '5% - 7.5%',
+    description: 'Aromática, intensa y con amargor marcado.'
   },
   {
+    slug: 'lager',
     name: 'Lager',
     icon: '🍺',
-    description: 'Ligera, limpia y refrescante.'
+    range: 'IBU bajo',
+    profile: 'Refrescante',
+    alcohol: '4% - 5.5%',
+    description: 'Ligera, limpia y fácil de beber.'
   },
   {
+    slug: 'stout',
     name: 'Stout',
     icon: '☕',
-    description: 'Oscura, tostada y con notas a café o chocolate.'
+    range: 'Cuerpo alto',
+    profile: 'Tostada',
+    alcohol: '4% - 8%',
+    description: 'Oscura, cremosa y con notas a café o chocolate.'
   },
   {
+    slug: 'porter',
     name: 'Porter',
     icon: '🍫',
-    description: 'Maltosa, suave y con carácter tostado.'
+    range: 'Maltosa',
+    profile: 'Tostada',
+    alcohol: '4.5% - 6.5%',
+    description: 'Suave, oscura y con carácter a malta tostada.'
   },
   {
+    slug: 'wheat',
     name: 'Wheat',
     icon: '🌾',
-    description: 'Suave, fresca y con cuerpo ligero.'
+    range: 'Cuerpo medio',
+    profile: 'Suave',
+    alcohol: '4% - 5.5%',
+    description: 'Fresca, ligera y elaborada con trigo.'
   },
   {
+    slug: 'sour',
     name: 'Sour',
     icon: '🍒',
-    description: 'Ácida, frutal y muy expresiva.'
+    range: 'Ácida',
+    profile: 'Frutal',
+    alcohol: '3% - 6%',
+    description: 'Expresiva, ácida y muchas veces frutal.'
   }
 ]
 </script>
